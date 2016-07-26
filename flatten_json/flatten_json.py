306038c399d766ac714cdd1d6483e01826a59cc6
Alexander Zaitsev <alexander.zaitsev@targetprocess.com>
2016-07-26 17:14:04 +0300

    custom delimiters

--- flatten_json/flatten_json.py
+++ flatten_json/flatten_json.py
@@ -1,14 +1,14 @@
 def flatten_json(y):
     out = {}
 
-    def flatten(x, name=''):
+    def flatten(x, name='', delimiter='_'):
         if type(x) is dict:
             for a in x:
-                flatten(x[a], name + a + '_')
+                flatten(x[a], name + a + delimiter)
         elif type(x) is list:
             i = 0
             for a in x:
-                flatten(a, name + str(i) + '_')
+                flatten(a, name + str(i) + delimiter)
                 i += 1
         else:
             a = ''
