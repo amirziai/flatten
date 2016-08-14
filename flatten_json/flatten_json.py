0921cbca7ecc243b90e9707533923ca3a35b6005
Michael Mattsson <michael.mattsson@nimblestorage.com>
2016-08-14 12:34:30 -0700

    Add prefix and delimiter as params (typofix)

--- flatten_json/flatten_json.py
+++ flatten_json/flatten_json.py
@@ -4,11 +4,11 @@ def flatten_json(y, **kwargs):
     def flatten(x, name=kwargs.get('prefix','')):
         if type(x) is dict:
             for a in x:
-                flatten(x[a], name + a + kwargs.get('delimeter', '_'))
+                flatten(x[a], name + a + kwargs.get('delimiter', '_'))
         elif type(x) is list:
             i = 0
             for a in x:
-                flatten(a, name + str(i) + kwargs.get('delimeter', '_'))
+                flatten(a, name + str(i) + kwargs.get('delimiter', '_'))
                 i += 1
         else:
             a = ''
