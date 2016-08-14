29c01174033d586773eab6b01dcd31431c5f8776
Michael Mattsson <michael.mattsson@nimblestorage.com>
2016-08-14 12:29:25 -0700

    Add prefix and delimeter as params

--- flatten_json/flatten_json.py
+++ flatten_json/flatten_json.py
@@ -1,14 +1,14 @@
-def flatten_json(y):
+def flatten_json(y, **kwargs):
     out = {}
 
-    def flatten(x, name=''):
+    def flatten(x, name=kwargs.get('prefix','')):
         if type(x) is dict:
             for a in x:
-                flatten(x[a], name + a + '_')
+                flatten(x[a], name + a + kwargs.get('delimeter', '_'))
         elif type(x) is list:
             i = 0
             for a in x:
-                flatten(a, name + str(i) + '_')
+                flatten(a, name + str(i) + kwargs.get('delimeter', '_'))
                 i += 1
         else:
             a = ''
