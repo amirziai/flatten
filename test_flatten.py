9bdcbd834288449d7dd9b6ebdff3eeaa865507ef
amirziai <arziai@gmail.com>
2017-02-18 14:41:46 -0800

    Adding unflatten,

--- test_flatten.py
+++ test_flatten.py
@@ -1,13 +1,13 @@
 import unittest
 
-from flatten_json import flatten_json
+from flatten_json import flatten, unflatten
 
 
 class UnitTests(unittest.TestCase):
     def test_no_flatten(self):
         dic = {'a': '1', 'b': '2', 'c': 3}
         expected = dic
-        actual = flatten_json(dic)
+        actual = flatten(dic)
         self.assertEqual(actual, expected)
 
     def test_one_flatten(self):
@@ -16,7 +16,7 @@ class UnitTests(unittest.TestCase):
                'c': {'c1': '3', 'c2': '4'}
                }
         expected = {'a': '1', 'b': '2', 'c_c1': '3', 'c_c2': '4'}
-        actual = flatten_json(dic)
+        actual = flatten(dic)
         self.assertEqual(actual, expected)
 
     def test_custom_separator(self):
@@ -25,7 +25,7 @@ class UnitTests(unittest.TestCase):
                'c': {'c1': '3', 'c2': '4'}
                }
         expected = {'a': '1', 'b': '2', 'c*c1': '3', 'c*c2': '4'}
-        actual = flatten_json(dic, '*')
+        actual = flatten(dic, '*')
         self.assertEqual(actual, expected)
 
     def test_list(self):
@@ -34,7 +34,7 @@ class UnitTests(unittest.TestCase):
             'b': [{'c': [2, 3]}]
         }
         expected = {'a': 1, 'b_0_c_0': 2, 'b_0_c_1': 3}
-        actual = flatten_json(dic)
+        actual = flatten(dic)
         self.assertEqual(actual, expected)
 
     def test_list_and_dict(self):
@@ -44,7 +44,7 @@ class UnitTests(unittest.TestCase):
             'c': [{'d': [2, 3, 4], 'e': [{'f': 1, 'g': 2}]}]
         }
         expected = {'a': 1, 'b': 2, 'c_0_d_0': 2, 'c_0_d_1': 3, 'c_0_d_2': 4, 'c_0_e_0_f': 1, 'c_0_e_0_g': 2}
-        actual = flatten_json(dic)
+        actual = flatten(dic)
         self.assertEqual(actual, expected)
 
     def test_blog_example(self):
@@ -55,9 +55,41 @@ class UnitTests(unittest.TestCase):
         }
         expected = {'a': 1, 'b': 2, 'c_0_d_0': '2', 'c_0_d_1': 3, 'c_0_d_2': 4, 'c_0_e_0_f': 1,
                     'c_0_e_0_g': 2}
-        actual = flatten_json(dic)
+        actual = flatten(dic)
         self.assertEqual(actual, expected)
 
+    def test_unflatten_no_list(self):
+        dic = {
+            'a': 1,
+            'b_a': 2,
+            'b_b': 3,
+            'c_a_b': 5
+        }
+        expected = {
+            'a': 1,
+            'b': {'a': 2, 'b': 3},
+            'c': {'a': {'b': 5}}
+        }
+        actual = unflatten(dic)
+        self.assertEqual(actual, expected)
+
+    def test_unflatten_with_list(self):
+        """This test is currently failing, unflatten is not handling lists properly"""
+        dic = {
+            'a': 1,
+            'b_0': 1,
+            'b_1': 2,
+            'c_a': 'a',
+            'c_b_0': 1,
+            'c_b_1': 2
+        }
+        expected = {
+            'a': 1,
+            'b': [1, 2],
+            'c': {'a': 'a', 'b': [1, 2]}
+        }
+        actual = unflatten(dic)
+        self.assertEqual(actual, expected)
 
 if __name__ == '__main__':
     unittest.main()
