1c9d4f27eba0e443c2d0c1fbea6f833f29d714b1
amirziai <arziai@gmail.com>
2017-12-22 12:40:22 -0800

    consecutive check is now a generator

--- util.py
+++ util.py
@@ -5,5 +5,5 @@ def check_if_numbers_are_consecutive(list_):
     :param list_: list of integers
     :return: Boolean
     """
-    return all([True if second - first == 1 else False
-                for first, second in zip(list_[:-1], list_[1:])])
+    return all((True if second - first == 1 else False
+                for first, second in zip(list_[:-1], list_[1:])))
